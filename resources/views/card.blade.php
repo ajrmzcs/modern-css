f0591b31bed47eafca6ab276897189d45ecc502e
Antonio Ramirez <ajrmzcs@gmail.com>
2020-04-07 10:39:29 -0500

    Lesson 3

---
+++ resources/views/card.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+
+    <link rel="stylesheet" href="/css/normalize.css">
+
+    <link rel="stylesheet" href="/css/card.css">
+
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="card">
+
+        {{--left--}}
+        <div class="card-left">
+            <a href="#" class="card-skill-button">PHP</a>
+
+            <img
+                src="https://laracasts.s3.amazonaws.com/series/thumbnails/testing-jargon.png"
+                alt="" width="120px" height="120px">
+
+            <span class="card-difficulty">Intermediate Difficulty</span>
+
+        </div>
+
+        {{--right--}}
+        <div class="card-right">
+            <h3 class="card-title"><a href="#">Testing Jargon</a></h3>
+            <p class="card-excerpt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem deserunt libero quod rem, vel
+                vero.
+            </p>
+            <div class="card-meta">
+                <div>
+                    <img src="/images/icons/books.svg" alt="" width="15px" height="15px">
+                    5 lessons
+                </div>
+                <div>
+                    <img src="/images/icons/clock.svg" alt="" width="15px" height="15px">
+                    53:10 min
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
